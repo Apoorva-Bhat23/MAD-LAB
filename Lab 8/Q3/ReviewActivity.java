54e5b1501545b651db6dd9e97cca9a3e7a356b56
Apoorva Y R <142596735+Apoorva-Bhat23@users.noreply.github.com>
2025-07-14 11:10:54 +0530

    Add files via upload

---
+++ Lab 8/Q3/ReviewActivity.java
@@ -0,0 +1,36 @@
+package com.example.moviereview;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReviewActivity extends AppCompatActivity {
+    TextView txtDetails;
+    DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+        txtDetails = findViewById(R.id.txtDetails);
+        dbHelper = new DatabaseHelper(this);
+
+        String movieName = getIntent().getStringExtra("movieName");
+        displayMovieDetails(movieName);
+    }
+
+    private void displayMovieDetails(String movieName) {
+        Cursor cursor = dbHelper.getMovieDetails(movieName);
+        if (cursor.moveToFirst()) {
+            String details = "Movie Name: " + cursor.getString(1) + "\n" +
+                    "Year: " + cursor.getInt(2) + "\n" +
+                    "Rating: " + cursor.getInt(3) + "/5\n" +
+                    "Review: " + cursor.getString(4);
+            txtDetails.setText(details);
+        }
+        cursor.close();
+    }
+}
